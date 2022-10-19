8e32d050a7965f3c837f61d9def3588312b93be7
Celia Methia <Celia Methia@Celia.columbus.rr.com>
2022-10-19 19:01:52 -0400

    my project

---
+++ src/main/java/amazon/Books.java
@@ -0,0 +1,21 @@
+package amazon;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Books {
+	WebDriver driver;
+
+	@FindBy(linkText="Deals in Books") WebElement DB;
+	
+	public void  DealBooks() { //create the method
+		 DB.click();
+	}
+	public Books(WebDriver driver) { //create the constructor
+		 this.driver=driver;
+		 PageFactory.initElements(driver, this); 
+	}	
+}
+// to get data back we need to create the object of this propreties
